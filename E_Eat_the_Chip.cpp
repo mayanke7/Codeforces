35a398f5d751c86eadbbbf0cc8322208c9e8c10d
mayanke7 <mayankubz@hotmail.com>
2024-06-01 16:32:12 +0530

    Bulk commit of competitive programming problems

---
+++ E_Eat_the_Chip.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define LL long long 
+#define DEB(x) cout << #x << "=" << x << endl
+#define DEB2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define PB push_back
+#define F first
+#define S second
+#define ALL(x) x.begin(), x.end()
+typedef pair<int, int>  PII;
+typedef pair<LL, LL>    PLL;
+const int MOD = 1000000007;
+#define PI 3.1415926535897932384626
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    LL t;
+    cin>>t;
+    while(t--){
+        int h,w,a,b,c,d;
+        cin>>h>>w>>a>>b>>c>>d;
+
+        if(abs(a-c)&1){
+            //alice
+            
+        }else{
+            //bob
+
+        }
+    
+    }
+}
+
+
+
